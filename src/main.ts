95d8e4623cce518230497b69746bf95a27e078ac
Ed Frey <edwinf@users.noreply.github.com>
2021-02-10 18:52:31 -0500

    feat: initial implementation with terraform plan command

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,51 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from '@actions/core';
+import { exec, ExecOptions } from '@actions/exec';
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const terraformArgs: string = core.getInput('args');
+    //const githubToken: string = core.getInput('token');
+    const workingDirectory: string = core.getInput('workingDirectory');
+    const debug: boolean =
+      (core.getInput('debug', { required: false }) || 'false') === 'true';
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    let stdOut = '';
+    let stdErr = '';
 
-    core.setOutput('time', new Date().toTimeString())
+    const options: ExecOptions = {};
+    options.listeners = {
+      stdout: (data: Buffer) => {
+        stdOut += data.toString();
+      },
+      stderr: (data: Buffer) => {
+        stdErr += data.toString();
+      },
+    };
+    options.cwd = workingDirectory;
+    // don't escape the args cause
+    options.windowsVerbatimArguments = true;
+    if (debug) {
+      if (options.env != null) {
+        options.env['TF_LOG'] = 'DEBUG';
+      } else {
+        options.env = { TF_LOG: 'DEBUG' };
+      }
+    }
+
+    core.debug(`Starting terraform plan at ${new Date().toTimeString()}`);
+
+    await exec('terraform', ['plan', '--no-color', terraformArgs], options);
+
+    core.debug(`Terraform plan completed at ${new Date().toTimeString()}`);
+    core.debug(' ------ Standard Out from Plan -----');
+    core.debug(stdOut);
+    core.debug(' ------ Standard Out from Plan -----');
+    core.debug(' ------ Standard Error from Plan -----');
+    core.debug(stdErr);
+    core.debug(' ------ Standard Error from Plan -----');
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
 }
 
-run()
+run();
