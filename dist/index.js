117777bcab802b5c340ae37840ca24107d855a45
Alex Lillie <Alex.Lillie@hyland.com>
2023-05-19 10:30:35 -0400

    feat: attempt to see if this works

--- dist/index.js
+++ dist/index.js
@@ -150,7 +150,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __importStar(__nccwpck_require__(2186));
 const exec_1 = __nccwpck_require__(1514);
-const io_1 = __importDefault(__nccwpck_require__(7436));
+//import io from '@actions/io';
+const io = __importStar(__nccwpck_require__(7436));
 const fs_1 = __importDefault(__nccwpck_require__(7147));
 const path_1 = __importDefault(__nccwpck_require__(1017));
 const github_1 = __nccwpck_require__(5928);
@@ -223,7 +224,7 @@ function writeFile(directory, output, error) {
     return __awaiter(this, void 0, void 0, function* () {
         //const io = require('@actions/io');
         console.log("BEFORE THE mkdirP");
-        yield io_1.default.mkdirP(directory);
+        yield io.mkdirP(directory);
         console.log("AFTER THE mkdirP");
         yield fs_1.default.promises.writeFile(path_1.default.join(directory, 'std.out'), output);
         yield fs_1.default.promises.writeFile(path_1.default.join(directory, 'std.err'), error);
